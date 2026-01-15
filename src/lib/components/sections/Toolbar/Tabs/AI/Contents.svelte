1fc35a49e12ff602e2166077f31cb4ba069a0541
もいずる <momizibe@outlook.jp>
2026-01-15 13:13:07 +0900

    ファイル名の修正

---
+++ src/lib/components/sections/Toolbar/Tabs/AI/Contents.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { BgRemoval } from "./functions";
+</script>
+
+<div class="w-full h-full flex flex-col justify-center lg:justify-start items-center">
+    <BgRemoval />
+</div>
---
+++ src/lib/components/sections/Toolbar/Tabs/AI/tab.svelte.ts
@@ -0,0 +1,9 @@
+import AIIcon from "$lib/assets/icons/ai.svelte";
+import Contents from "./Contents.svelte";
+
+export const aiInfo = {
+    id: "ai",
+    name: "AI",
+    icon: AIIcon,
+    contents: Contents
+};
