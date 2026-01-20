4e614b147cc1a7cc63673e53ad8739714e60abfd
もいずる <momizibe@outlook.jp>
2026-01-20 14:02:27 +0900

    Update opencv-filters.worker.ts

--- src/lib/worker/opencv-filters.worker.ts
+++ src/lib/worker/opencv-filters.worker.ts
@@ -1,5 +1,7 @@
 import cv from "@techstark/opencv-js";
 
+const BLUR_MAX = 100;
+
 self.addEventListener('message', async (e) => {
     const { imageData, blurFactor } = e.data;
     if (!imageData) { return; }
@@ -7,7 +9,7 @@ self.addEventListener('message', async (e) => {
     const src = cv.matFromImageData(imageData);
 
     if (blurFactor !== 0) {
-        const size = Math.round(((blurFactor * 17) - 1) / 2) * 2 + 1;
+        const size = Math.round(((blurFactor * BLUR_MAX) - 1) / 2) * 2 + 1;
         cv.GaussianBlur(src, src, new cv.Size(size, size), 0, 0, cv.BORDER_DEFAULT);
     }
 
